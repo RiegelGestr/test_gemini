3965b0baf0c30fbd44eee9b3a3ca1c667153b933
Antonio Desiderio <43544849+RiegelGestr@users.noreply.github.com>
2024-05-20 14:48:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -203,4 +203,4 @@ def stop_server(n_clicks):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8050))
-    app.run_server(debug=True, port=port, host='0.0.0.0')
+    app.run_server(debug=False, host= '0.0.0.0')
